12f5ef5bd901440cfccec4be4e7f58a59021e53f
liushiru <shiruliu55@hotmail.com>
2020-01-19 13:02:00 +0800

    fix human, unrecyclable bugs

--- location_results.py
+++ location_results.py
@@ -2,7 +2,7 @@ from chat_action_util import send_typing_action
 from telegram.ext import ConversationHandler
 import math
 
-# 'contaminated_plastic','glass', 'human', 'pak','paper',  'plastic','wrapper'
+# 'contaminated_plastic','glass',, 'pak','paper',  'plastic','wrapper'
 
 
 METAL_KEYWORDS = ["metal", "cans"]
@@ -36,7 +36,8 @@ CLOTHES_LOCATION = [
 
 PAPER_KEYWORDS = ["notes", "book", "bookes", "paper", "cardboard"]
 
-HUMAN = (1.297030, 103.773765)
+HUMAN_KEYWORDS = ["human"]
+HUMAN_LOCATION = (1.297030, 103.773765, "Don't give up!")
 
 ALL_KEYWORDS = E_WASTE_KEYWORDS + PEN_KEYWORDS + CLOTHES_KEYWORDS + MISSLEADING_KEYWORDS
 
@@ -47,67 +48,80 @@ ALL_LOCATION = [E_WASTE_BIZ, E_WASTE_SOC, E_WASTE_NUH,
 @send_typing_action
 def get_all_results(update, context):
     trash_label = context.user_data['trash'].lower()
-    update.message.reply_text(
-        "You can recycle " + trash_label + " here")
-
-    if trash_label in E_WASTE_KEYWORDS:
-        for location in E_WASTE_LOCATION:
-            update.message.reply_text(location[2])
-            context.bot.send_location(
-                chat_id=update.effective_chat.id, latitude=location[0], longitude=location[1])
-
-    if trash_label in PEN_KEYWORDS:
-        for location in PEN_LOCATION:
-            update.message.reply_text(location[2])
-            context.bot.send_location(
-                chat_id=update.effective_chat.id, latitude=location[0], longitude=location[1])
-
-    if trash_label in CLOTHES_KEYWORDS:
-        for location in CLOTHES_LOCATION:
-            update.message.reply_text(location[2])
-            context.bot.send_location(
-                chat_id=update.effective_chat.id, latitude=location[0], longitude=location[1])
 
     if trash_label in MISSLEADING_KEYWORDS:
-    update.message.reply_text(
-        "Though it looks like it can be recyle, " + trash_label + " is actually unrecyclable. You can throw it in any dust bin")
+        update.message.reply_text(
+            "Though it looks like it can be recyle, " + trash_label + " is actually unrecyclable. You can throw it in any dust bin")
 
-    if trash_label not in ALL_KEYWORDS:
+    elif trash_label not in ALL_KEYWORDS:
         update.message.reply_text(
             "Looks like" + trash_label + "cannot be recycled in NUS" + "Please email Office of Facilities Management (OFM) at ofmhelp@nus.edu.sg or call the OFM Helpdesk at 6516 1515, to request for additional recycling bins.")
 
+    else: 
+        update.message.reply_text(
+            "You can recycle " + trash_label + " here")
+
+        if trash_label in E_WASTE_KEYWORDS:
+            for location in E_WASTE_LOCATION:
+                update.message.reply_text(location[2])
+                context.bot.send_location(
+                    chat_id=update.effective_chat.id, latitude=location[0], longitude=location[1])
+
+        if trash_label in PEN_KEYWORDS:
+            for location in PEN_LOCATION:
+                update.message.reply_text(location[2])
+                context.bot.send_location(
+                    chat_id=update.effective_chat.id, latitude=location[0], longitude=location[1])
+
+        if trash_label in CLOTHES_KEYWORDS:
+            for location in CLOTHES_LOCATION:
+                update.message.reply_text(location[2])
+                context.bot.send_location(
+                    chat_id=update.effective_chat.id, latitude=location[0], longitude=location[1])
+        
+        if trash_label in HUMAN_KEYWORDS:
+            for location in HUMAN_LOCATION:
+                update.message.reply_text(location[2])
+                context.bot.send_location(
+                    chat_id=update.effective_chat.id, latitude=location[0], longitude=location[1])
+    
     del context.user_data['trash']
     return ConversationHandler.END
 
 
 def get_results_by_location(update, context):
     trash_label = context.user_data['trash'].lower()
-    update.message.reply_text(
-        "You can recycle " + trash_label + " here")
-
-    target = update.message.location
-
-    if trash_label in E_WASTE_KEYWORDS:
-        location_tuple = choose_shortest_location(target, E_WASTE_LOCATION)
-
-    if trash_label in PEN_KEYWORDS:
-        location_tuple = choose_shortest_location(target, PEN_LOCATION)
-
-    if trash_label in CLOTHES_KEYWORDS:
-        location_tuple = choose_shortest_location(target, CLOTHES_LOCATION)
-
-    update.message.reply_text(location_tuple[2])
-
-    context.bot.send_location(
-        chat_id=update.effective_chat.id, latitude=location_tuple[0], longitude=location_tuple[1])
 
     if trash_label in MISSLEADING_KEYWORDS:
-    update.message.reply_text(
-        "Though it looks like it can be recyle, " + trash_label + " is actually unrecyclable. You can throw it in any dust bin")
+        update.message.reply_text(
+            "Though it looks like it can be recyle, " + trash_label + " is actually unrecyclable. You can throw it in any dust bin")
 
-    if trash_label not in ALL_KEYWORDS:
+    elif trash_label not in ALL_KEYWORDS:
         update.message.reply_text(
             "Looks like" + trash_label + "cannot be recycled in NUS" + "Please email Office of Facilities Management (OFM) at ofmhelp@nus.edu.sg or call the OFM Helpdesk at 6516 1515, to request for additional recycling bins.")
+
+    else:
+        update.message.reply_text(
+            "You can recycle " + trash_label + " here")
+
+        target = update.message.location
+
+        if trash_label in E_WASTE_KEYWORDS:
+            location_tuple = choose_shortest_location(target, E_WASTE_LOCATION)
+
+        if trash_label in PEN_KEYWORDS:
+            location_tuple = choose_shortest_location(target, PEN_LOCATION)
+
+        if trash_label in CLOTHES_KEYWORDS:
+            location_tuple = choose_shortest_location(target, CLOTHES_LOCATION)
+
+        if trash_label in HUMAN_KEYWORDS:
+            location_tuple = choose_shortest_location(target, HUMAN_LOCATION)
+        update.message.reply_text(location_tuple[2])
+
+        context.bot.send_location(
+            chat_id=update.effective_chat.id, latitude=location_tuple[0], longitude=location_tuple[1])
+
 
 
 
